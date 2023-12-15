f5f2c15ba7c7a0c23f8a58b8578030539dfd7f31
Eugenio Bernardi <e.bernardi@tudelft.nl>
2023-12-15 14:50:20 +0100

    model method and TZ

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"  # TODO: change to local NL time zone
+TIME_ZONE = "Europe/Amsterdam"  # TODO: change to local NL time zone
 
 USE_I18N = True
 
