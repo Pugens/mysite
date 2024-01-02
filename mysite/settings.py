5cbc19eeb5db3d033554977a4679fd4d8287c666
Eugenio Bernardi <e.bernardi@tudelft.nl>
2024-01-02 12:58:47 +0100

    template index

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Amsterdam"  # TODO: change to local NL time zone
+TIME_ZONE = "Europe/Amsterdam"
 
 USE_I18N = True
 
